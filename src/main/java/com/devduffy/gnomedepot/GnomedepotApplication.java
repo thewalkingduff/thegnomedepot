dbd8e89368580d387f1da8966bf8364c4a796064
thewalkingduff <senorduff@gmail.com>
2022-11-21 23:23:54 -0500

    initial commit

---
+++ src/main/java/com/devduffy/gnomedepot/GnomedepotApplication.java
@@ -0,0 +1,13 @@
+package com.devduffy.gnomedepot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnomedepotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnomedepotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devduffy/gnomedepot/GnomedepotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devduffy.gnomedepot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnomedepotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
